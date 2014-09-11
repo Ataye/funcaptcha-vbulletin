2c7479634e93d6e0efeb966923393334c08020ba
Kevin Gosschalk <kevin@swipeads.co>
2014-09-11 14:49:53 +1000

    vb updates

--- README.md
+++ README.md
@@ -3,6 +3,4 @@ funcaptcha-vbulletin
 
 vBulletin plugin for FunCaptcha.
 
-This plugin enables you to use FunCaptcha to project your vBulletin forum from spam.
-
-Please use the folder appropriate for your version of vBulletin. See documentation.html for a full installation guide for each version in their folders.
+This plugin enables you to use FunCaptcha to project your vBulletin forum from spam. See documentation.html for a full installation guide for each version in their folders.
--- upload/includes/class_humanverify_funcaptcha.php
+++ upload/includes/class_humanverify_funcaptcha.php
@@ -14,6 +14,7 @@ define( 'FUNCAPTCHA_LABEL', $vbulletin->options["funcaptcha_label"]);
 define( 'FUNCAPTCHA_THEME', $vbulletin->options["funcaptcha_theme"]);
 define( 'FUNCAPTCHA_PROXY', $vbulletin->options["funcaptcha_proxy"]);
 define( 'FUNCAPTCHA_JSFALLBACK', $vbulletin->options["funcaptcha_jsfallback"]);
+define( 'FUNCAPTCHA_NEWPOST', $vbulletin->options["funcaptcha_newpost"]);
 
 require_once(DIR . '/includes/funcaptcha.php');
 
@@ -66,56 +67,59 @@ class vB_HumanVerify_FunCaptcha extends vB_HumanVerify_Abstract
 	 */
 	function output_token($var_prefix = 'humanverify')
 	{
-		$popup_button = null;
-		$funcaptcha =  new FUNCAPTCHA();
-		$funcaptcha->setSecurityLevel(FUNCAPTCHA_SECURITY_LEVEL);		
-		$funcaptcha->setProxy(FUNCAPTCHA_PROXY);
-		$funcaptcha->setTheme(FUNCAPTCHA_THEME);
-		$funcaptcha->setNoJSFallback(FUNCAPTCHA_JSFALLBACK);
+		if (FUNCAPTCHA_PUBLIC_KEY && FUNCAPTCHA_PRIVATE_KEY) {
+			$output = "<p>CAPTCHA not setup correctly, please contact this sites administrator.</p>";
+		} else {
+			$funcaptcha =  new FUNCAPTCHA();
+			$funcaptcha->setSecurityLevel(FUNCAPTCHA_SECURITY_LEVEL);		
+			$funcaptcha->setProxy(FUNCAPTCHA_PROXY);
+			$funcaptcha->setTheme(FUNCAPTCHA_THEME);
+			$funcaptcha->setNoJSFallback(FUNCAPTCHA_JSFALLBACK);
 
-		$is_reply = false;
-		$is_topic = false;
-		$target_element = "";
-		switch ($var_prefix) {
-			case "editor_reply" :
-				$is_reply = true;
-			break;
-			case "editor_thread" :
-				$is_topic = true;
-			break;
+			$is_reply = false;
+			$is_topic = false;
+			$target_element = "";
+			switch ($var_prefix) {
+				case "editor_reply" :
+					$is_reply = true;
+				break;
+				case "editor_thread" :
+					$is_topic = true;
+				break;
+			}
+
+			// If a topic or reply FC, we need to move it from the original position.
+			$script = "";
+			if ($is_topic || $is_reply) {
+	        	$script =   "<script type='text/javascript'>
+	                    var moved = false;
+	                    // This ensures the code is executed in the right order
+	                    if (!moved) {
+	                        rearrange_form_elements();
+	                    } else {
+	                        setTimeout('rearrange_form_elements()', 1000);
+	                    }
+	                    function rearrange_form_elements() {
+	                        var target = document.getElementById('funcaptcha-wrapper');
+	                        if (target != null && document.getElementsByName('vbform')[0] != null) {
+	                            target.parentNode.removeChild(target);
+	                            document.getElementsByName('vbform')[0].appendChild(target);
+	                        }
+	                    }
+	                </script>";
+	        }
+			
+			//only show HTML/label if not lightbox mode.
+			$output = "<div class=\"blockrow\"><input type=hidden value='1' id='humanverify' name='humanverify' /><div class=\"group\"><li>";
+			$output = $output . "<div id='funcaptcha-wrapper'>";
+			if (FUNCAPTCHA_LABEL && FUNCAPTCHA_LABEL != ""){
+	            $output = $output . "<label>".FUNCAPTCHA_LABEL."</label>";
+	        }
+			$output = $output . $funcaptcha->getFunCaptcha(FUNCAPTCHA_PUBLIC_KEY, array("logs" => array("newpost" => FUNCAPTCHA_NEWPOST)));
+			$output = $output . "</li></div></div></div>";
+			$output = $output . $script;
 		}
-
-
-		// If a topic or reply FC, we need to move it from the original position.
-		$script = "";
-		if ($is_topic || $is_reply) {
-        	$script =   "<script type='text/javascript'>
-                    var moved = false;
-                    // This ensures the code is executed in the right order
-                    if (!moved) {
-                        rearrange_form_elements();
-                    } else {
-                        setTimeout('rearrange_form_elements()', 1000);
-                    }
-                    function rearrange_form_elements() {
-                        var target = document.getElementById('funcaptcha-wrapper');
-                        if (target != null && document.getElementsByName('vbform')[0] != null) {
-                            target.parentNode.removeChild(target);
-                            document.getElementsByName('vbform')[0].appendChild(target);
-                        }
-                    }
-                </script>";
-        }
 		
-		//only show HTML/label if not lightbox mode.
-		$output = "<div class=\"blockrow\"><input type=hidden value='1' id='humanverify' name='humanverify' /><div class=\"group\"><li>";
-		$output = $output . "<div id='funcaptcha-wrapper'>";
-		if (FUNCAPTCHA_LABEL && FUNCAPTCHA_LABEL != ""){
-            $output = $output . "<label>".FUNCAPTCHA_LABEL."</label>";
-        }
-		$output = $output . $funcaptcha->getFunCaptcha(FUNCAPTCHA_PUBLIC_KEY);
-		$output = $output . "</li></div></div></div>";
-		$output = $output . $script;
 		return $output;
 	}
 }
